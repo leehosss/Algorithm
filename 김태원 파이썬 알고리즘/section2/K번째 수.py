03a23dbcbba24e16dd4adc66122025e7cdfdb13d
Lee Hosung <leehosung503@naver.com>
2026-06-17 16:40:41 +0900

    K번째 수

---
+++ 김태원 파이썬 알고리즘/section2/K번째 수.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open("input.txt","r")
+
+t = int(input())
+for i in range(t):
+    n,s,e,k = map(int, input().split())
+    a = list(map(int, input().split()))
+    a = a[s-1:e]
+    a.sort()
+    print("#%d %d" %(i+1,a[k-1]))  
