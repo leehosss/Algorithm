801126c826657b4de80e12b8c08c11c8ff1fa2f8
Lee Hosung <leehosung503@naver.com>
2026-06-22 13:38:44 +0900

    순열 구하기

---
+++ 김태원 파이썬 알고리즘/section6/순열 구하기.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open("input.txt","r")
+from collections import deque
+
+
+def DFS(l):
+    global cnt
+    if l == m:
+        for i in range(m):  
+            print(res[i], end = " ")
+        print()
+        cnt +=1
+    else:
+        for i in range(1,n+1):
+            if ch[i] == 0:
+                ch[i] = 1
+                res[l] = i
+                DFS(l+1)
+                ch[i] = 0
+    
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    res = [0]*m
+    ch = [0]*(n+1)
+    cnt = 0
+    DFS(0)
+    print(cnt)
